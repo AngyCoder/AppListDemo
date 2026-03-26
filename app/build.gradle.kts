af15c1203f41fb64700d714939ee4f3c8d6e4e8b
AngyCoder <a.kuncevich25@gmail.com>
2026-03-26 19:09:28 +0300

    build: добавить зависимости Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,13 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    // Gson
+    implementation("com.google.code.gson:gson:2.10.1")
 }
